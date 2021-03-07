86a3bd3325a961c7c09ec8c014e6e7ffd14f15f5
park-youngjae <79900983+park-youngjae@users.noreply.github.com>
2021-03-08 03:16:49 +0900

    Update detect.py

--- detect.py
+++ detect.py
@@ -98,7 +98,7 @@ if __name__ == '__main__':
     parser.add_argument('--image_folder',default='./input', help='path to image_folder which contains text images')
     parser.add_argument('--workers', type=int, help='number of data loading workers', default=0)
     parser.add_argument('--batch_size', type=int, default=192, help='input batch size')
-    parser.add_argument('--saved_model', default='./pretrained/7.pth', help="path to saved_model to evaluation")
+    parser.add_argument('--saved_model', default='./pretrained/Fine-Tuned.pth', help="path to saved_model to evaluation")
     """ Data processing """
     parser.add_argument('--batch_max_length', type=int, default=25, help='maximum-label-length')
     parser.add_argument('--imgH', type=int, default=32, help='the height of the input image')
